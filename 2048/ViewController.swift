0c8832f0434ab1c81c05f8c4dc4c579c0722ac83
Cheuk Lun Ko <cheuklun.ko@icloud.com>
2019-01-22 19:04:40 +0000

    hello

--- 2048/ViewController.swift
+++ 2048/ViewController.swift
@@ -14,6 +14,13 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    var numbers = [Int]()
+    
+    func printLabel(int: Int) {
+        print(String(int))
+    }
+    
 
 
 }
